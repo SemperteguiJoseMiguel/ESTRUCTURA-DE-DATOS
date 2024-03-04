c2a4bf7c8767ec39c2e3040af3fe0ea0fd179744
SemperteguiJoseMiguel <159058674+SemperteguiJoseMiguel@users.noreply.github.com>
2024-03-04 13:43:05 -0400

    Create estudiante.h

---
+++ estudiante.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Estudiante
+{
+private:
+	string nombre;
+	int edad;
+	string materia;
+	float calificacion;
+public:
+	Estudiante();
+	~Estudiante();
+	void set_nombre(string n);
+	string get_nombre{};
+
+};
