e09d21353ca0e11f7374803dde949d2a23d4e7d5
SemperteguiJoseMiguel <159058674+SemperteguiJoseMiguel@users.noreply.github.com>
2024-03-04 16:45:16 -0400

    VectorEj1.cpp

---
+++ VectorEj1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "vector.h"
+
+using namespace std;
+
+vector::vector(void)
+{
+	vec[10] = 0;
+	n = 0;
+}
+
+
+vector::~vector(void)
+{
+}
+void vector::set_n(int _n) {
+	n = _n;
+}
+int vector::get_n() {
+	return n;
+}
+
+void vector::cargarVector(int vec[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "vec[" << i << "] =";
+		cin >> vec[i];
+	}
+}
+
+void vector::mostrarVector(int vec[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << vec[i] << ", ";
+	}
+	cout << endl;
+}
+
+void vector::ordenarVector(int vec[], int n) {
+	int aux;
+	for (int i = 0; i < (n - 1); i++) {
+		for (int j = i; j < n; j++) {
+			if (vec[i] > vec[j]) {
+				aux = vec[i];
+				vec[i] = vec[j];
+				vec[j] = aux;
+			}
+		}
+	}
+}
