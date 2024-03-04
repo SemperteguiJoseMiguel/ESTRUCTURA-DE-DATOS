5f6206b93f3ec793f6c78c4e48f06dccb25c55b5
SemperteguiJoseMiguel <159058674+SemperteguiJoseMiguel@users.noreply.github.com>
2024-03-04 16:45:57 -0400

    VectorEj1.h

---
+++ VectorEj1.h
@@ -0,0 +1,13 @@
+class vector
+{
+private:
+	int vec[10], n;
+public:
+	vector(void); // constructor
+	~vector(void); //destructor
+	void set_n(int _n);
+	int get_n();
+	void cargarVector(int vec[], int n); //metodos
+	void mostrarVector(int vec[], int n);
+	void ordenarVector(int vec[], int n);
+};
